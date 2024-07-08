78535a976c3a95be70df6b81eb23a8125b413058
Hwang <10221092@gmail.com>
2024-07-08 19:05:17 +0700

    Upload

--- main.py
+++ main.py
@@ -1,7 +1,118 @@
-from bts7960 import rpi_JGB37545 
-mt = rpi_JGB37545.motor(hall_sensor=17, 
-           bts_L_EN=13, 
-           bts_R_EN=19, 
-           bts_L_PWM=6, 
-           bts_R_PWM=12, 
-           wheel_diameter = 0.1)
+import RPi.GPIO as gpio
+import time
+
+def main():
+    #Create an instance of the drivers
+    drivers = Drivers()
+    #Test all of the functions
+    drivers.Test_All()
+    #Stop the motors
+    drivers.Stop()
+
+class Drivers():
+    def __init__(self):
+        
+        #Enable forward motor on the drivers
+        self.D1_R_EN = 13;
+        self.D2_R_EN = 16;
+
+        #Enable revers motor on the drivers
+        self.D1_L_EN = 6;
+        self.D2_L_EN = 12;
+
+        #Forward & Backward for Driver 1
+        self.D1_RPWM = 26;
+        self.D1_LPWM = 19;
+
+        #Forward & Backward for Driver 2
+        self.D2_RPWM = 21;
+        self.D2_LPWM = 20;
+
+#       Define all parameters for the drivers
+        self.GPIO_PARAMETERS();
+        
+        self.rpwm_D1 = gpio.PWM(self.D1_RPWM, 100)
+        self.rpwm_D2 = gpio.PWM(self.D2_RPWM, 100)
+
+        self.lpwm_D1 = gpio.PWM(self.D1_LPWM, 100)
+        self.lpwm_D2 = gpio.PWM(self.D2_LPWM, 100)
+
+        self.rpwm_D1.start(0)
+        self.rpwm_D2.start(0)
+
+        self.lpwm_D2.start(0)
+        self.lpwm_D1.start(0)        
+
+
+
+    def GPIO_PARAMETERS(self):
+        gpio.setmode(gpio.BCM)
+        gpio.setwarnings(False)
+        #Set all of our pins to output
+        ##############################
+        #Enable the DRIVER 1 PINS
+        gpio.setup(self.D1_LPWM, gpio.OUT)
+        gpio.setup(self.D1_RPWM, gpio.OUT)
+        gpio.setup(self.D1_L_EN, gpio.OUT)
+        gpio.setup(self.D1_R_EN, gpio.OUT)
+
+        #Enable the DRIVER 2 PINS
+        gpio.setup(self.D2_RPWM, gpio.OUT)
+        gpio.setup(self.D2_R_EN, gpio.OUT)
+        gpio.setup(self.D2_L_EN, gpio.OUT)
+        gpio.setup(self.D2_LPWM, gpio.OUT)
+
+        #Enable Left & Right mpvement
+        #DRIVER 1
+        gpio.output(self.D1_L_EN, True)
+        gpio.output(self.D2_L_EN, True)
+        gpio.output(self.D1_R_EN, True)
+        gpio.output(self.D2_R_EN, True)
+
+
+    def Backward(self, speed):
+        self.rpwm_D1.ChangeDutyCycle(speed)
+        self.rpwm_D2.ChangeDutyCycle(speed)
+        #time.sleep(.15)
+        #self.rpwm_D1.ChangeDutyCycle(0)
+        #self.rpwm_D2.ChangeDutyCycle(0)
+        
+           
+    #def Rotate_Right(self):
+        #self.rpwm_D2.ChangeDutyCycle(50)
+        #time.sleep(.15)
+        #self.rpwm_D2.ChangeDutyCycle(0)
+        
+    
+    #def Rotate_Left(self):
+        #self.rpwm_D1.ChangeDutyCycle(50)
+        #time.sleep(.15)
+        #self.rpwm_D1.ChangeDutyCycle(0)
+        
+        
+    def Forward(self,speed):
+        self.lpwm_D1.ChangeDutyCycle(speed)
+        self.lpwm_D2.ChangeDutyCycle(speed)
+        #time.sleep(.15)
+        #self.lpwm_D1.ChangeDutyCycle(0)
+        #self.lpwm_D2.ChangeDutyCycle(0)
+
+   
+        
+    def Stop(self):
+        self.lpwm_D1.ChangeDutyCycle(0)
+        self.lpwm_D2.ChangeDutyCycle(0)
+        self.rpwm_D1.ChangeDutyCycle(0)
+        self.rpwm_D2.ChangeDutyCycle(0)
+        time.sleep(.15)
+    
+
+    def Test_All(self):
+        print("Testing backward movement")
+        self.Backward()
+        print("Testing Forward movement")
+        self.Forward()
+        print("Testing Left movement");
+        self.Rotate_Left();
+        print("Testing Rigth movement");
+        self.Rotate_Right()
