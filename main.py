7033bec78f1bf5a2b513dd9bb71d2a4ef12ca870
Hwang <10221092@gmail.com>
2024-07-08 19:11:12 +0700

    4

--- main.py
+++ main.py
@@ -13,34 +13,18 @@ class Drivers():
     def __init__(self):
         
         #Enable forward motor on the drivers
-        self.D1_R_EN = 13;
-        self.D2_R_EN = 16;
-
+        self.D1_R_EN = 13
         #Enable revers motor on the drivers
-        self.D1_L_EN = 6;
-        self.D2_L_EN = 12;
-
+        self.D1_L_EN = 6
         #Forward & Backward for Driver 1
-        self.D1_RPWM = 26;
-        self.D1_LPWM = 19;
-
-        #Forward & Backward for Driver 2
-        self.D2_RPWM = 21;
-        self.D2_LPWM = 20;
+        self.D1_RPWM = 26
+        self.D1_LPWM = 19
 
 #       Define all parameters for the drivers
-        self.GPIO_PARAMETERS();
-        
+        self.GPIO_PARAMETERS()
         self.rpwm_D1 = gpio.PWM(self.D1_RPWM, 100)
-        self.rpwm_D2 = gpio.PWM(self.D2_RPWM, 100)
-
         self.lpwm_D1 = gpio.PWM(self.D1_LPWM, 100)
-        self.lpwm_D2 = gpio.PWM(self.D2_LPWM, 100)
-
         self.rpwm_D1.start(0)
-        self.rpwm_D2.start(0)
-
-        self.lpwm_D2.start(0)
         self.lpwm_D1.start(0)        
 
 
@@ -56,23 +40,14 @@ class Drivers():
         gpio.setup(self.D1_L_EN, gpio.OUT)
         gpio.setup(self.D1_R_EN, gpio.OUT)
 
-        #Enable the DRIVER 2 PINS
-        gpio.setup(self.D2_RPWM, gpio.OUT)
-        gpio.setup(self.D2_R_EN, gpio.OUT)
-        gpio.setup(self.D2_L_EN, gpio.OUT)
-        gpio.setup(self.D2_LPWM, gpio.OUT)
-
         #Enable Left & Right mpvement
         #DRIVER 1
         gpio.output(self.D1_L_EN, True)
-        gpio.output(self.D2_L_EN, True)
         gpio.output(self.D1_R_EN, True)
-        gpio.output(self.D2_R_EN, True)
 
 
     def Backward(self, speed):
         self.rpwm_D1.ChangeDutyCycle(speed)
-        self.rpwm_D2.ChangeDutyCycle(speed)
         #time.sleep(.15)
         #self.rpwm_D1.ChangeDutyCycle(0)
         #self.rpwm_D2.ChangeDutyCycle(0)
@@ -92,7 +67,6 @@ class Drivers():
         
     def Forward(self,speed):
         self.lpwm_D1.ChangeDutyCycle(speed)
-        self.lpwm_D2.ChangeDutyCycle(speed)
         #time.sleep(.15)
         #self.lpwm_D1.ChangeDutyCycle(0)
         #self.lpwm_D2.ChangeDutyCycle(0)
@@ -101,9 +75,7 @@ class Drivers():
         
     def Stop(self):
         self.lpwm_D1.ChangeDutyCycle(0)
-        self.lpwm_D2.ChangeDutyCycle(0)
         self.rpwm_D1.ChangeDutyCycle(0)
-        self.rpwm_D2.ChangeDutyCycle(0)
         time.sleep(.15)
     
 
@@ -112,7 +84,4 @@ class Drivers():
         self.Backward()
         print("Testing Forward movement")
         self.Forward()
-        print("Testing Left movement");
-        self.Rotate_Left();
-        print("Testing Rigth movement");
-        self.Rotate_Right()
+        print("Testing Left movement")
